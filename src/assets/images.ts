f9ed2f35cd8a8ff8dc4ccc2f0b2b7fa115579ee5
mohamadelhinamy <mohmadelhinamy7@gmail.com>
2022-07-11 14:45:12 +0200

    marvel assets and the images exporting file

---
+++ src/assets/images.ts
@@ -0,0 +1,23 @@
+const marvelBg = require('./marvelBg.webp')
+const dcBg = require('./dcBg.jpeg')
+const thor = require('./thor.png')
+const captainAmerica = require('./captainAmerica.png')
+const hulk = require('./hulk.png')
+const wanda = require('./wanda.png')
+const doctorStrange = require('./doctorStrange.png')
+const ironman = require('./ironman.png')
+const blackWidow = require('./blackWidow.png')
+const blackPanther = require('./blackPanther.png')
+
+export {
+  marvelBg,
+  dcBg,
+  thor,
+  captainAmerica,
+  hulk,
+  wanda,
+  doctorStrange,
+  ironman,
+  blackPanther,
+  blackWidow,
+}
